4c69d60990e2aad08e74d27e3f3ab6a6b0adea41
Amery <l45756859@gmail.com>
2025-02-13 13:23:16 +0800

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Enzo test</title>
+        <title>Enzo and Amery test</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
