b17352d9081eefa253f7efacf3a85517f2bf7517
Amery <l45756859@gmail.com>
2025-02-17 14:17:55 +0800

    events

---
+++ resources/views/components/events.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  
+
+</head>
+<body>
+    <section class="news">
+        <h1 class="events">Events</h1>
+    <div class="card">
+        <img src="https://scontent.fceb2-2.fna.fbcdn.net/v/t39.30808-6/475857935_1096299049175804_369178114615784792_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=aywVT8tIjYMQ7kNvgFoie32&_nc_oc=AdjBp6CdLhfbl0ToQytlAGCNZC-dWJh7YI3vkH2z_BAZS-GyHMLd4hHh7RikInUfa94&_nc_zt=23&_nc_ht=scontent.fceb2-2.fna&_nc_gid=Aze0U8XvroK_b-WGnq6Z73s&oh=00_AYAmCQunA2bwyvYgUNsJCgnLqqhHHqwrvMWGc2I-2RCN3w&oe=67B89081" alt="">    
+        <h3>Title</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo et quae consequatur excepturi nostrum exercitationem fugit maiores, maxime cumque distinctio magnam animi ipsam, modi dolorem molestiae quisquam! Omnis, natus.</p>
+        </div>
+    <div class="card">
+        <img src="https://scontent.fceb6-2.fna.fbcdn.net/v/t39.30808-6/477601239_1102357918569917_4951403480113378367_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=109&ccb=1-7&_nc_sid=f727a1&_nc_ohc=1Uus7IMo0AYQ7kNvgFOOvAV&_nc_oc=AdgsE2i7iV1oqDO2saZiQKqUK0cLnwJsgJSN_-Wnm4sygB2jseng8PfoQq-ggsOjlIA&_nc_zt=23&_nc_ht=scontent.fceb6-2.fna&_nc_gid=A8CFCjlJzfNdGKJ3JR0ClyC&oh=00_AYBciIVp20QR_-uYO9WvoFtl_BAID53VEM2h-qxfMB9VfQ&oe=67B8A863" alt="">
+        <h3>Title</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo et quae consequatur excepturi nostrum exercitationem fugit maiores, maxime cumque distinctio magnam animi ipsam, modi dolorem molestiae quisquam! Omnis, natus.</p>
+
+    </div>
+    <div class="card">
+    <img src="https://scontent.fceb2-2.fna.fbcdn.net/v/t39.30808-6/477578843_1102356431903399_3110628929506937709_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_ohc=dlGFjc-26gcQ7kNvgHVrgtE&_nc_oc=AdgFkW2YaH627kyXw6bCTymhtPIxSwFtZUyeKP6GN1CJjjt6exMf2IP3mPQhAKkVzvU&_nc_zt=23&_nc_ht=scontent.fceb2-2.fna&_nc_gid=A8CFCjlJzfNdGKJ3JR0ClyC&oh=00_AYAGG47oVOKvIYaLkdclncPIRnAELo8NL9-EnnNeeWKd_g&oe=67B8B7B3" alt="">
+    <h3>Title</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo et quae consequatur excepturi nostrum exercitationem fugit maiores, maxime cumque distinctio magnam animi ipsam, modi dolorem molestiae quisquam! Omnis, natus.</p>
+
+    </div>
+    <div class="card">
+    <img src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/476469614_1102358788569830_3767365203870224067_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_ohc=p5BIwtHQpHQQ7kNvgGrTjRT&_nc_oc=AdidhoI3Lwjme6tkoD6dTL8Gaxe1bZf6WjaeA2JqBVk5esQZOuyIMt6xEGLUPfUWplQ&_nc_zt=23&_nc_ht=scontent.fceb6-1.fna&_nc_gid=A8CFCjlJzfNdGKJ3JR0ClyC&oh=00_AYDfJO9WezHOLgMZ8BlBK3QrUHzLn6RZhUlTaC2pawmSGg&oe=67B8BCAB" alt="">
+    <h3>Title</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo et quae consequatur excepturi nostrum exercitationem fugit maiores, maxime cumque distinctio magnam animi ipsam, modi dolorem molestiae quisquam! Omnis, natus.</p>
+
+    </div>
+    <div class="card">
+    <img src="https://scontent.fceb2-2.fna.fbcdn.net/v/t39.30808-6/477578843_1102356431903399_3110628929506937709_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_ohc=dlGFjc-26gcQ7kNvgHVrgtE&_nc_oc=AdgFkW2YaH627kyXw6bCTymhtPIxSwFtZUyeKP6GN1CJjjt6exMf2IP3mPQhAKkVzvU&_nc_zt=23&_nc_ht=scontent.fceb2-2.fna&_nc_gid=A8CFCjlJzfNdGKJ3JR0ClyC&oh=00_AYAGG47oVOKvIYaLkdclncPIRnAELo8NL9-EnnNeeWKd_g&oe=67B8B7B3" alt="">
+    <h3>Title</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo et quae consequatur excepturi nostrum exercitationem fugit maiores, maxime cumque distinctio magnam animi ipsam, modi dolorem molestiae quisquam! Omnis, natus.</p>
+
+    </div>
+    <div class="card">
+    <img src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/476053448_1098525738953135_6981154836632336238_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=6HZ5KAxA818Q7kNvgEjzYVY&_nc_oc=Adhrt47kw8sJmuv1UMZzMR7MsBDuQyeGSRuJxCXJfnMRLz1nqpzNpIPQ5_ljIbcH4L0&_nc_zt=23&_nc_ht=scontent.fceb6-1.fna&_nc_gid=AsjU-2hNi2YH4xh1kik8ekO&oh=00_AYCX35gVNCU6MvG4wtztcs4HufcU6aaeDOOz_D5K7pBIdA&oe=67B8B4A1" alt="">
+    <h3>Title</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo et quae consequatur excepturi nostrum exercitationem fugit maiores, maxime cumque distinctio magnam animi ipsam, modi dolorem molestiae quisquam! Omnis, natus.</p>
+    </div>
+    </section>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<<<<<<< HEAD
         <title>Enzo and Amery test</title>
-=======
         <title>CPNC</title>
->>>>>>> 7945124d7ac821ec9075d023d343a8b3ca06f68d
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="stylesheet" href="{{ asset('css/events.css') }}">
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -23,5 +20,6 @@
     </head>
    <body>
             <x-nav></x-nav>
+            <x-events></x-events>
     </body>
 </html>
