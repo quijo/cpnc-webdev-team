a3aa901f617bfb902fecf5cfb62d677432dd5f7b
PermaXi <futbolero319@gmail.com>
2025-02-17 14:09:02 +0800

    create hero

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,7 @@
+<section class="hero">
+    <div class="herodesc">
+        <h2>Reflecting Brilliance Transforming Generations</h2>
+        <p>We are committed to mentor transformational leaders of the next generation. We exist to provide quality Christian education, to recognize God's call and to serve the community with excellence.</p>
+        <a href="#" class="learnmore">Learn More</a>
+    </div>
+</section>
