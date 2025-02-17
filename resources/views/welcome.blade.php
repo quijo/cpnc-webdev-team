7945124d7ac821ec9075d023d343a8b3ca06f68d
“[John <[rjohnquia@gmail.com]>
2025-02-17 11:01:05 +0800

    update main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Enzo test</title>
+        <title>CPNC</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
