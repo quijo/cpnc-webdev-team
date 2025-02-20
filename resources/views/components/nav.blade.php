f693f077e587e4062008c20c4bffb2496f95a4a2
“[John <[rjohnquia@gmail.com]>
2025-02-20 10:34:16 +0800

    add color variable

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -2,8 +2,9 @@
     <nav>
        <a href="#" class="logo">CPNC</a>
        <ul class="menu">
-        <li><a href="#">home</a></li>
-        <li><a href="#">about</a></li>
+            <li><a href="#">home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">contact</a></li>
        </ul>
     </nav>
 </header>
