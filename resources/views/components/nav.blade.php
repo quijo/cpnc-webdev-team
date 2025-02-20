e2028ffc177ac41b3445cce9e92ea385db694ca6
“[John <[rjohnquia@gmail.com]>
2025-02-20 10:13:48 +0800

    upadate event css and navigation bar

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,3 +1,9 @@
-<nav>
-   <a href="#">CPNC</a>
-</nav>
+<header>
+    <nav>
+       <a href="#" class="logo">CPNC</a>
+       <ul class="menu">
+        <li><a href="#">home</a></li>
+        <li><a href="#">about</a></li>
+       </ul>
+    </nav>
+</header>
