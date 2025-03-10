380bd30a57e65cc6a403c48c7b57c8308c7f7e09
PermaXi <futbolero319@gmail.com>
2025-03-10 14:00:27 +0800

    updated academics

--- resources/views/components/academics.blade.php
+++ resources/views/components/academics.blade.php
@@ -1,6 +1,20 @@
 <section class="academics">
     <h1 class="acadtitle">Academics</h1>
-    <div class="acadhero">
+    <div class="acadhero"></div>
+    <div class="pushinp">
+        Over six decades, CPNC has stood as a pillar of academic excellence in the Central Philippine region. 
+        Our dedicated faculty and staff have tirelessly worked to create an environment that fosters intellectual 
+        curiosity, critical thinking, and a passion for lifelong learning.
     </div>
-    <div class="pushinp">Over six decades, CPNC has stood as a pillar of academic excellence in the Central Philippine region. Our dedicated faculty and staff have tirelessly worked to create an environment that fosters intellectual curiosity, critical thinking, and a passion for lifelong learning.</div>
+    <div class="educationtype">
+    <div class="basiced">
+        <div class="basicedimg"></div>
+        <p class="edu-text">We offer education from Nursery, Kindergarten, Elementary, and Junior High School.</p>
+    </div>
+    <div class="collegeed">
+        <div class="collegeimg"></div>
+        <p class="edu-text">In our college, we offer Religious Education courses and a Bachelor of Arts in Theology.</p>
+    </div>
+</div>
+
 </section>
