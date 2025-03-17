b5cb1abef324a835fd7040f130c80dbcf37f64b2
PermaXi <futbolero319@gmail.com>
2025-03-17 14:40:03 +0800

    added changes

---
+++ resources/views/components/acads.blade.php
@@ -0,0 +1,27 @@
+<section class="acads">
+    <h1 class="academicstitle">Academics</h1>
+    <div class="acadshero">
+        <img src="{{ asset('images/acadhero.jpg') }}" alt="">
+    </div>
+    <div class="acadbarintro">
+        <h1>Central Philippine Nazarene College
+        Your Gateway to Excellence in Education!</h1>
+    </div>
+    <div class="academiccards">
+        <div class="acadcard">
+            <img src="{{ asset('images/acadcard1.jpg') }}" alt="">
+            <h3>Academic Excellence</h3>
+            <p>Our curriculum is designed to challenge and inspire students, fostering critical thinking, creativity, and a passion for knowledge. With a team of dedicated and experienced educators, we are committed to providing a top-notch academic experience.</p>
+        </div>
+        <div class="acadcard">
+            <img src="{{ asset('images/acadcard2.jpg') }}" alt="">
+            <h3>Holistic Development</h3>
+            <p>Beyond academics, we focus on the holistic development of each student. Our programs encompass not only intellectual growth but also character building, emotional intelligence, and social skills. We aim to mold well-rounded individuals who are prepared for success in all facets of life.</p>
+        </div>
+        <div class="acadcard">
+            <img src="{{ asset('images/acadcard3.jpg') }}" alt="">
+            <h3>Faith-Based Education</h3>
+            <p>At <strong>Central Philippine Nazarene College</strong>, we integrate Christian values into every aspect of our educational approach. Our commitment to faith-based education ensures that students not only excel academically but also develop a strong moral compass, empathy, and a sense of purpose.</p>
+        </div>       
+    </div>
+</section>
