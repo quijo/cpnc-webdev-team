681037da00ed705cc2cf8934462857fb2ef3d4ec
Amery <l45756859@gmail.com>
2025-03-26 23:14:35 +0800

    update about us

--- resources/views/components/aboutus.blade.php
+++ resources/views/components/aboutus.blade.php
@@ -1,56 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>About Us</title>
-    <link rel="stylesheet" href="public.css.an">
-
-</head>
-<body>
-    <h1>About Us</h1>
+<h1>About Us</h1>
     <section class="aboutus">
-        <div class="mainimg">
-        </div>
+        <div id="upimg"><img src="{{ asset ('images/aboutusimg.jpg')}}"></div>
         <div class="boxes">
-            <!-- first small box -->
-
-            <div class="wtext1">
-
-                <img src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/476469614_1102358788569830_3767365203870224067_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFgyVirJflXF58Ew5KHHwcTN5eTP0VTG7I3l5M_RVMbsmyhTGes39Pq90RO14T8OISkaV6Ll11kCAPNTcD2ruJl&_nc_ohc=NZTxuHZM0ZwQ7kNvgG8SxL6&_nc_oc=AdiK5UB9aQHggYAnmZuvE5D8RRp6rNrLIvfDn24wdqYrx4hvV1fuaotgzxC_FPzbPQM&_nc_zt=23&_nc_ht=scontent.fceb6-1.fna&_nc_gid=A5ojHMJbND3Rlw7JFyBuo6U&oh=00_AYCK4Otk383h7SUYk9l287rgUk_kXhNRWQFKgHg7HYso7w&oe=67BC78EB" class="smallimg">
-                <div class="texts">
-                    <h3>History</h3>
-                    <p>Founded in 1968, CPNC emerged from a vision to provide quality education rooted in the principles of the Church of the Nazarene. </p>
-                </div>
-            </div>
-
-            <!-- second small box -->
-            <div class="wtext1">
-                <img src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/476469614_1102358788569830_3767365203870224067_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFgyVirJflXF58Ew5KHHwcTN5eTP0VTG7I3l5M_RVMbsmyhTGes39Pq90RO14T8OISkaV6Ll11kCAPNTcD2ruJl&_nc_ohc=NZTxuHZM0ZwQ7kNvgG8SxL6&_nc_oc=AdiK5UB9aQHggYAnmZuvE5D8RRp6rNrLIvfDn24wdqYrx4hvV1fuaotgzxC_FPzbPQM&_nc_zt=23&_nc_ht=scontent.fceb6-1.fna&_nc_gid=A5ojHMJbND3Rlw7JFyBuo6U&oh=00_AYCK4Otk383h7SUYk9l287rgUk_kXhNRWQFKgHg7HYso7w&oe=67BC78EB" class="smallimg">
-                <div class="texts">
-                    <p>The institution has grown from humble beginnings into a beacon of academic excellence, producing graduates who excel in their fields and are dedicated to Christian service.</p>
-                </div>
-            </div>
-                <!-- third small box -->
-
-            <div class="wtext1">
-                <img src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/476469614_1102358788569830_3767365203870224067_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFgyVirJflXF58Ew5KHHwcTN5eTP0VTG7I3l5M_RVMbsmyhTGes39Pq90RO14T8OISkaV6Ll11kCAPNTcD2ruJl&_nc_ohc=NZTxuHZM0ZwQ7kNvgG8SxL6&_nc_oc=AdiK5UB9aQHggYAnmZuvE5D8RRp6rNrLIvfDn24wdqYrx4hvV1fuaotgzxC_FPzbPQM&_nc_zt=23&_nc_ht=scontent.fceb6-1.fna&_nc_gid=A5ojHMJbND3Rlw7JFyBuo6U&oh=00_AYCK4Otk383h7SUYk9l287rgUk_kXhNRWQFKgHg7HYso7w&oe=67BC78EB" class="smallimg">
-                <div class="texts">
-                    <p>Over the past six decades, CPNC has stood as a pillar of academic excellence in the Central Philippines region. </p>
-                </div>
-            </div>
+            <p class="text1">Founded in 1968, CPNC emerged from a vision to provide quality education rooted in the principles of the Church of the Nazarene. </p>
+            <p class="text1">The institution has grown from humble beginnings into a beacon of academic excellence, producing graduates who excel in their fields and are dedicated to Christian service.</p>
+            <p class="text1">Over the past six decades, CPNC has stood as a pillar of academic excellence in the Central Philippines region. </p>
         </div>
         <div class="textimg">
-            <div class="wtext2">
-                <img src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/476469614_1102358788569830_3767365203870224067_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFgyVirJflXF58Ew5KHHwcTN5eTP0VTG7I3l5M_RVMbsmyhTGes39Pq90RO14T8OISkaV6Ll11kCAPNTcD2ruJl&_nc_ohc=NZTxuHZM0ZwQ7kNvgG8SxL6&_nc_oc=AdiK5UB9aQHggYAnmZuvE5D8RRp6rNrLIvfDn24wdqYrx4hvV1fuaotgzxC_FPzbPQM&_nc_zt=23&_nc_ht=scontent.fceb6-1.fna&_nc_gid=A5ojHMJbND3Rlw7JFyBuo6U&oh=00_AYCK4Otk383h7SUYk9l287rgUk_kXhNRWQFKgHg7HYso7w&oe=67BC78EB" class="smallimg">
+            <div class="wtext">
+                <img src="{{asset ('images/staffs.jpg')}}" class="smallimg">
                 <div class="texts">
                     <h3>Mission</h3>
                     <p>Mentoring every generation of transformational leaders through quality holistic education and exemplary Christ-centered life that influence the church and the global community.</p>
                 </div>
             </div>
-            <div class="wtext2">
-                <img src="https://scontent.fceb6-1.fna.fbcdn.net/v/t39.30808-6/476469614_1102358788569830_3767365203870224067_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFgyVirJflXF58Ew5KHHwcTN5eTP0VTG7I3l5M_RVMbsmyhTGes39Pq90RO14T8OISkaV6Ll11kCAPNTcD2ruJl&_nc_ohc=NZTxuHZM0ZwQ7kNvgG8SxL6&_nc_oc=AdiK5UB9aQHggYAnmZuvE5D8RRp6rNrLIvfDn24wdqYrx4hvV1fuaotgzxC_FPzbPQM&_nc_zt=23&_nc_ht=scontent.fceb6-1.fna&_nc_gid=A5ojHMJbND3Rlw7JFyBuo6U&oh=00_AYCK4Otk383h7SUYk9l287rgUk_kXhNRWQFKgHg7HYso7w&oe=67BC78EB" class="smallimg">
+            <div class="wtext">
+                <img src="{{asset ('images/bgbofficers.jpg')}}" class="smallimg">
                 <div class="texts">
                     <h3>Vision</h3>
                     <p>A leading institution of excellent Christian education and creative interdisciplinary integration.</p>
